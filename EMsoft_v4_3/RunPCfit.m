7dbc530c6d5346da8fd60402cfd8802f305d5cd1
Edward Pang <53325250+epang22@users.noreply.github.com>
2020-02-21 19:37:23 -0500

    Add files via upload

--- EMsoft_v4_3/RunPCfit.m
+++ EMsoft_v4_3/RunPCfit.m
@@ -1,7 +1,7 @@
 %RUNPCFIT
-% Use this script to perform detector fitting
+% Use this script to fit the pattern center to a single pattern
 % Fill in INPUT PARAMETERS section with desired parameters
-% Last updated: 7/24/19 (Edward Pang, MIT)
+% 2/21/20 (Edward Pang, MIT)
 
 clear
 
@@ -12,56 +12,58 @@ addpath('minq5')
 
 %%% INPUT PARAMETERS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Define paths
-data.path = 'CuonCu/191217_Site8/PCfit/test/';  % path within EMdatapathname to find experimental patterns and to save data
+data.path = 'testdata/';  % path within EMdatapathname to find experimental patterns and to save data
 
 % Input data
-data.img_exp_name = 'OIM Map 1_01991.png';     % name of image file located within path
-data.binning = 2;        % binning mode (1, 2, 4, 8). Simulated image size will be numsx/binning x numsy/binning
+data.img_exp_name = 'EDAX-Ni_Scan1_x126y0.png';     % name of image file located within path
+data.binning = 8;        % binning mode (1, 2, 4, 8). Simulated image size will be numsx/binning x numsy/binning
 
 % Output data
-data.h5outputlogical = 1; % output h5 file? 1=yes, 0=no
-data.h5output = 'test3.h5';   % Name of output file (located in path)
-data.logoutputlogical = 1; % output log file of screen? 1=yes, 0=no
-data.logoutput = 'test3.txt';   % Name of output file (located in path)
+data.h5output = 'output.h5';   % Name of output file (located in path)
+data.logoutput = 'output.txt';   % Name of output file (located in path)
 
 % Starting guesses for PC and orientation
-data.L = 2508.08;    % in microns
-data.xpc = 8.2329;         % in px
-data.ypc = 49.1829;       % in px
-data.phi1 = 112.26;      % in deg in EMsoft convention
-data.PHI = 34.99;
-data.phi2 = 115.50;
+data.L = 15870.0234;    % in microns
+data.xpc = 3.48576;         % in px
+data.ypc = 114.20352;       % in px
+data.phi1 = 35.2655;      % in deg in EMsoft convention
+data.PHI = 109.96;
+data.phi2 = 205.439;
 
 % Define trust region to optimize within
-data.trust_L = 75;     % in microns (75 is a reasonable starting value)
-data.trust_xpc = 15;     % in px   (15 is a reasonable starting value)
-data.trust_ypc = 15;     % in px   (15 is a reasonable starting value)
-data.trust_angle = 3;   % in degrees    (3 is a reasonable starting value)
+data.trust_L = 500;     % in microns
+data.trust_xpc = 5;     % in px
+data.trust_ypc = 5;     % in px
+data.trust_angle = 1;   % in degrees
 
-%%% EMsoft parameters for simulating patterns and computing dot products
-data.energymin = 15.0;   % energy range in the intensity summation (keV)
-data.energymax = 30.0;
-data.masterfile = '200106_Cu_fcc_30kV_60deg_master/Cu_fcc-master-30kV.h5';   % master pattern input file, path relative to EMdatapathname
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% DO NOT EDIT BELOW THIS LINE (unless you know what you're doing) %
+% EMsoft parameters for the master pattern
+data.energymin = 10.0;   % energy range in the intensity summation (keV)
+data.energymax = 20.0;
+data.masterfile = 'DItutorial_Ni-master-20kV/Ni-master-20kV.h5';   % master pattern input file, path relative to EMdatapathname
 
 
-%%% Some extra parameters %%%
-% detector parameters (Harvard FIB crop 87%)
-data.thetac = 8;   % tilt angle of the camera (positive below horizontal, degrees)
-data.delta = 7.4*480/416;   % CCD pixel size on the scintillator surface (microns)
-data.numsx = 416;    % number of CCD pixels along x and y
-data.numsy = 416;
-data.omega = 0;      % angle between normal of sample and detector
 
-% Paths for this computer (don't need to touch after initial setup of EMsoft)
-data.homepath = '/home/eddie/EMsoftfiles/EMData/';  % path to EMdatapathname
-data.tmppath = 'tmp/';   % path within EMdatapathname for creation of temp files
+%%% Parameters you don't need to change often %%%
+% Paths for this computer
+data.homepath = '/home/eddie/EMsoftfiles/EMData/';  % path to EMdatapathname (don't need to touch after initial setup of EMsoft)
+data.tmppath = 'tmp/';   % path within EMdatapathname for creation of temp files (you need to manually create this directory)
 
-%%% Advanced parameters %%%
-data.imgprebinned = 1;    % Is image already binned (matches binning below)? 1=yes, 0=no
+% Is image already binned (matches binning below)? 1=yes, 0=no
+%   Allows you to further bin down patterns you read in
+data.imgprebinned = 1; 	% 1=yes (already binned, image size matches numsx/binning by numsy/binning), 0=no (you want to additionally bin)
+
+% Files to output
 data.h5outputlogical = 1; % output h5 file? 1=yes, 0=no
 data.logoutputlogical = 1; % output log file of screen? 1=yes, 0=no
+
+% Detector parameters
+data.thetac = 10;   % tilt angle of the camera (positive below horizontal, degrees)
+data.delta = 59.2;   % CCD pixel size on the scintillator surface (microns)
+data.numsx = 480;    % number of CCD pixels along x and y
+data.numsy = 480;
+data.omega = 0;      % angle between normal of sample and detector
+
+% Some EMsoft parameters for dot product computations
 data.scalingmode = 'gam'; % intensity scaling mode: 'not'=no scaling, 'lin'=linear, 'gam'=gamma
 data.gammavalue = 0.33;  % gamma correction factor
 data.hipassw = 0.05;    % hi pass filter w param; 0.05 is reasonable
@@ -70,20 +72,21 @@ data.nthreads = 1; % for EMEBSDDI
 data.maskpattern = 'y';        % use circular mask? y or n
 data.r = min(data.numsx,data.numsy)/(2*data.binning);        % radius of circular mask (after binning)
 
-%%% Define snobfit parameters
+%%% Some more SNOBFIT parameters
+%   Default values are fine in most cases, can use the FindParameters.m program to find the optimal parameters for your system
 % Characteristic length scale in each direction (for scaling)
-data.Lstep = 25;       % in microns
-data.xpcstep = 1.5;    % in px
-data.ypcstep = 1.5;    % in px
-data.anglestep = 0.34;  % in degrees
+data.Lstep = 225;       % in microns
+data.xpcstep = 2.25;    % in px
+data.ypcstep = 2.25;    % in px
+data.anglestep = 0.43;  % in degrees
 
 % Define minimum step size for each variable
-data.Lstep_min = 0.03;       % in microns
-data.xpcstep_min = 0.002;   % in px
-data.ypcstep_min = 0.002;   % in px
-data.anglestep_min = 0.0004; % in degrees
+data.Lstep_min = 0.25;       % in microns
+data.xpcstep_min = 0.0025;   % in px
+data.ypcstep_min = 0.0025;   % in px
+data.anglestep_min = 0.0005; % in degrees
 
-% snobfit optimization parameters
+% Optimization parameters (default values are generally fine)
 data.ncall = 1000;   % limit on the number of function calls
 data.iterstop = 100;   % stop after this number of consecutive iterations without improvement in fbest
 data.npoint = 12;   % number of random start points to be generated (default=dim+6)
@@ -91,7 +94,8 @@ data.nreq = 12;     % no. of points to be generated in each call to SNOBFIT (def
 data.p = 0.5;        % probability of generating a point of class 4 
                 % (probe random region of space, global instead of local)
 data.display = 'iter';  % how much info to print to screen (notify, final, off, iter)[same as fminsearch]
-%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DO NOT EDIT BELOW THIS LINE (unless you are doing code development) %
 
 
 
--- EMsoft_v4_3/RunPCfit_loop.m
+++ EMsoft_v4_3/RunPCfit_loop.m
@@ -1,14 +1,8 @@
 %RUNPCFIT_LOOP
-% Use this script to perform detector fitting
+% Use this script to fit pattern centers to a multiple patterns
 % Fill in INPUT PARAMETERS section with desired parameters
 % This version can loop through multiple images
-% Original: 7/24/19 (Edward Pang, MIT)
-% Change log:
-% -1/8/20 ELP: Read in data and print averages, check for existing files
-% initially
-% -1/30/20 ELP: Instead of requiring a separate euler.txt file, directly
-% input euler angles here. Save a .txt file containing the average pattern
-% center from these runs.
+% 2/21/20 (Edward Pang, MIT)
 
 clear
 
@@ -19,57 +13,63 @@ addpath('minq5')
 
 %%% INPUT PARAMETERS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Input data
-data.path = 'CuonCu/191217_Site8/PCfit/test/';  % path within EMdatapathname to find experimental patterns and to save data
+data.path = 'testdata/';  % path within EMdatapathname to find experimental patterns and to save data
 imagenames = [
-    "OIM Map 1_01991.png"
-%     "OIM Map 1_04986.png"
+    "EDAX-Ni_Scan1_x126y0.png"
+    "EDAX-Ni_Scan1_x126y2.png"
+    "EDAX-Ni_Scan1_x126y3.png"
     ];  % name of image files to loop through (located in path)
-data.binning = 2;        % binning mode (1, 2, 4, 8). Simulated image size will be numsx/binning x numsy/binning
+data.binning = 8;        % binning mode (1, 2, 4, 8). Simulated image size will be numsx/binning x numsy/binning
 
 % Define euler angles (each row phi1, PHI, phi2 in degrees)
 euler = [
-    112.26 34.99 115.50
-%     7.88 98.64 217.81
+	35.2655 109.96 205.439
+	35.2655 109.96 205.439
+	35.2655 109.96 205.439
     ];
 
 % Output data (outputs separate file for each image)
-outputprefix = 'PCfit_test3';    % output file will be [outputprefix]_[imagename(ii) without extension].h5/txt
+outputprefix = 'output';    % output file will be [outputprefix]_[imagename(ii) without extension].h5/txt
 
-%%% Starting guesses for PC (usually from Hough data)
-data.L = 2508.08;    % in microns
-data.xpc = 8.2329;         % in px
-data.ypc = 49.1829;       % in px
+% Starting guesses for PC (usually from Hough data)
+data.L = 15870.0234;    % in microns
+data.xpc = 3.48576;      % in px
+data.ypc = 114.20352;     % in px
 
 % Define trust region to optimize within
-data.trust_L = 75;     % in microns (75 is a reasonable starting value)
-data.trust_xpc = 15;     % in px   (15 is a reasonable starting value)
-data.trust_ypc = 15;     % in px   (15 is a reasonable starting value)
-data.trust_angle = 3;   % in degrees    (3 is a reasonable starting value)
+data.trust_L = 500;     % in microns
+data.trust_xpc = 5;     % in px
+data.trust_ypc = 5;     % in px
+data.trust_angle = 1;   % in degrees
 
-%%% EMsoft parameters for simulating patterns and computing dot products
-data.energymin = 15.0;   % energy range in the intensity summation (keV)
-data.energymax = 30.0;
-data.masterfile = '200106_Cu_fcc_30kV_60deg_master/Cu_fcc-master-30kV.h5';   % master pattern input file, path relative to EMdatapathname
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% DO NOT EDIT BELOW THIS LINE (unless you know what you're doing) %
+% EMsoft parameters for the master pattern
+data.energymin = 10.0;   % energy range in the intensity summation (keV)
+data.energymax = 20.0;
+data.masterfile = 'DItutorial_Ni-master-20kV/Ni-master-20kV.h5';   % master pattern input file, path relative to EMdatapathname
 
 
-%%% Some extra parameters %%%
-% detector parameters (Harvard FIB crop 87%)
-data.thetac = 8;   % tilt angle of the camera (positive below horizontal, degrees)
-data.delta = 7.4*480/416;   % CCD pixel size on the scintillator surface (microns)
-data.numsx = 416;    % number of CCD pixels along x and y
-data.numsy = 416;
-data.omega = 0;      % angle between normal of sample and detector
 
-% Paths for this computer (don't need to touch after initial setup of EMsoft)
-data.homepath = '/home/eddie/EMsoftfiles/EMData/';  % path to EMdatapathname
-data.tmppath = 'tmp/';   % path within EMdatapathname for creation of temp files
+%%% Parameters you don't need to change often %%%
+% Paths for this computer
+data.homepath = '/home/eddie/EMsoftfiles/EMData/';  % path to EMdatapathname (don't need to touch after initial setup of EMsoft)
+data.tmppath = 'tmp/';   % path within EMdatapathname for creation of temp files  (you need to manually create this directory)
 
-%%% Advanced parameters %%%
-data.imgprebinned = 1;    % Is image already binned (matches binning below)? 1=yes, 0=no
+% Is image already binned (matches binning below)? 1=yes, 0=no
+%   Allows you to further bin down patterns you read in
+data.imgprebinned = 1; 	% 1=yes (already binned, image size matches numsx/binning by numsy/binning), 0=no (you want to additionally bin)
+
+% Files to output
 data.h5outputlogical = 1; % output h5 file? 1=yes, 0=no
 data.logoutputlogical = 1; % output log file of screen? 1=yes, 0=no
+
+% Detector parameters
+data.thetac = 10;   % tilt angle of the camera (positive below horizontal, degrees)
+data.delta = 59.2;   % CCD pixel size on the scintillator surface (microns)
+data.numsx = 480;    % number of CCD pixels along x and y
+data.numsy = 480;
+data.omega = 0;      % angle between normal of sample and detector
+
+% Some EMsoft parameters for dot product computations
 data.scalingmode = 'gam'; % intensity scaling mode: 'not'=no scaling, 'lin'=linear, 'gam'=gamma
 data.gammavalue = 0.33;  % gamma correction factor
 data.hipassw = 0.05;    % hi pass filter w param; 0.05 is reasonable
@@ -78,20 +78,21 @@ data.nthreads = 1; % for EMEBSDDI
 data.maskpattern = 'y';        % use circular mask? y or n
 data.r = min(data.numsx,data.numsy)/(2*data.binning);        % radius of circular mask (after binning)
 
-%%% Define snobfit parameters
+%%% Some more SNOBFIT parameters
+%   Default values are fine in most cases, can use the FindParameters.m program to find the optimal parameters for your system
 % Characteristic length scale in each direction (for scaling)
-data.Lstep = 25;       % in microns
-data.xpcstep = 1.5;    % in px
-data.ypcstep = 1.5;    % in px
-data.anglestep = 0.34;  % in degrees
+data.Lstep = 225;       % in microns
+data.xpcstep = 2.25;    % in px
+data.ypcstep = 2.25;    % in px
+data.anglestep = 0.43;  % in degrees
 
 % Define minimum step size for each variable
-data.Lstep_min = 0.03;       % in microns
-data.xpcstep_min = 0.002;   % in px
-data.ypcstep_min = 0.002;   % in px
-data.anglestep_min = 0.0004; % in degrees
+data.Lstep_min = 0.25;       % in microns
+data.xpcstep_min = 0.0025;   % in px
+data.ypcstep_min = 0.0025;   % in px
+data.anglestep_min = 0.0005; % in degrees
 
-% snobfit optimization parameters
+% Optimization parameters (default values are generally fine)
 data.ncall = 1000;   % limit on the number of function calls
 data.iterstop = 100;   % stop after this number of consecutive iterations without improvement in fbest
 data.npoint = 12;   % number of random start points to be generated (default=dim+6)
@@ -99,7 +100,9 @@ data.nreq = 12;     % no. of points to be generated in each call to SNOBFIT (def
 data.p = 0.5;        % probability of generating a point of class 4 
                 % (probe random region of space, global instead of local)
 data.display = 'iter';  % how much info to print to screen (notify, final, off, iter)[same as fminsearch]
-%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DO NOT EDIT BELOW THIS LINE (unless you are doing code development) %
+
 
 
 N = length(imagenames);     % number of patterns to test
